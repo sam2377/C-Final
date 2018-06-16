93b490c80a1924849ef68498ab7568b818754214
sam2377 <samcheng168@gmail.com>
2018-06-17 00:32:31 +0800

    加入專案檔案。

---
+++ Final/Form1.Designer.cs
@@ -0,0 +1,305 @@
+﻿namespace Final
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.checkedListBox2 = new System.Windows.Forms.CheckedListBox();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(39, 25);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1028, 556);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.button5);
+            this.tabPage1.Controls.Add(this.button4);
+            this.tabPage1.Controls.Add(this.comboBox2);
+            this.tabPage1.Controls.Add(this.comboBox1);
+            this.tabPage1.Controls.Add(this.button3);
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.checkedListBox2);
+            this.tabPage1.Controls.Add(this.checkedListBox1);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.textBox3);
+            this.tabPage1.Controls.Add(this.textBox2);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1020, 527);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "首頁";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // checkedListBox2
+            // 
+            this.checkedListBox2.FormattingEnabled = true;
+            this.checkedListBox2.Location = new System.Drawing.Point(547, 236);
+            this.checkedListBox2.Name = "checkedListBox2";
+            this.checkedListBox2.Size = new System.Drawing.Size(451, 164);
+            this.checkedListBox2.TabIndex = 8;
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(547, 14);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(451, 184);
+            this.checkedListBox1.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(265, 45);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(198, 160);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "新增資料";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(134, 180);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 25);
+            this.textBox3.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(134, 118);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 25);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox1
+            // 
+            this.textBox1.ImeMode = System.Windows.Forms.ImeMode.On;
+            this.textBox1.Location = new System.Drawing.Point(134, 56);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 25);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(55, 183);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "屬性(F/D):";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(55, 121);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "價位:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(55, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "名稱:";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.dataGridView2);
+            this.tabPage2.Controls.Add(this.dataGridView1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1020, 527);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "資料表";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(6, 284);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowTemplate.Height = 27;
+            this.dataGridView2.Size = new System.Drawing.Size(1008, 228);
+            this.dataGridView2.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(6, 6);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 27;
+            this.dataGridView1.Size = new System.Drawing.Size(1008, 262);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(632, 446);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(103, 30);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "決定食物";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(811, 446);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(103, 30);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "決定飲料";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(58, 300);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(151, 23);
+            this.comboBox1.TabIndex = 11;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(58, 376);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(151, 23);
+            this.comboBox2.TabIndex = 12;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(253, 281);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(92, 59);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "刪除食物";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(253, 357);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(92, 59);
+            this.button5.TabIndex = 14;
+            this.button5.Text = "刪除飲料";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1079, 608);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.CheckedListBox checkedListBox2;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Final/Form1.cs
@@ -0,0 +1,332 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace Final
+{
+    public partial class Form1 : Form
+    {
+
+        List<String> food = new List<string>();
+        List<String> drink = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            checkedListBox1.Items.Clear();
+            checkedListBox2.Items.Clear();
+            comboBox1.Items.Clear();
+            comboBox2.Items.Clear();
+            comboBox1.Text = "";
+            comboBox2.Text = "";
+            String selectCommand1 = "SELECT * FROM Food";
+            String selectCommand2 = "SELECT * FROM Drink";
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;" +
+                "AttachDbFilename=|DataDirectory|Food.mdf;" +
+                 "Integrated Security=True";
+            cn.Open();
+            DataSet ds1 = new DataSet();
+            SqlDataAdapter daScore1 = new SqlDataAdapter(selectCommand1, cn);
+            daScore1.Fill(ds1);
+            DataSet ds2 = new DataSet();
+            SqlDataAdapter daScore2 = new SqlDataAdapter(selectCommand2, cn);
+            daScore2.Fill(ds2);
+
+            dataGridView1.DataSource = ds1.Tables[0];
+            dataGridView2.DataSource = ds2.Tables[0];
+
+            for (int i = 0; i < dataGridView1.RowCount - 1; i++)
+            {
+                checkedListBox1.Items.Add(dataGridView1[0, i].Value);
+                comboBox1.Items.Add(dataGridView1[0, i].Value);
+            }
+            for (int i = 0; i < dataGridView2.RowCount - 1; i++)
+            {
+                checkedListBox2.Items.Add(dataGridView2[0, i].Value);
+                comboBox2.Items.Add(dataGridView2[0, i].Value);
+            }
+            for(int i = 0; i < checkedListBox1.Items.Count; i++)
+            {
+                checkedListBox1.SetItemChecked(i, true);
+            }
+            for(int i = 0; i < checkedListBox2.Items.Count; i++)
+            {
+                checkedListBox2.SetItemChecked(i, true);
+            }
+
+            cn.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!textBox1.Text.Equals("") && !textBox2.Text.Equals("") && textBox3.Text.Equals("F") || textBox3.Text.Equals("D"))
+            {
+                try
+                {
+                    int price = int.Parse(textBox2.Text);
+                    MessageBox.Show("添加成功");
+
+                    switch (textBox3.Text)
+                    {
+                        case "F":
+                            try
+                            {
+                                SqlConnection cn = new SqlConnection();
+                                cn.ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;" +
+                                    "AttachDbFilename=|DataDirectory|Food.mdf;" +
+                                    "Integrated Security=True";
+                                cn.Open();
+                                String sqlstr = "INSERT INTO Food(Id,Price) VALUES(@p1,@p2)";
+
+                                SqlCommand cmd = new SqlCommand(sqlstr, cn);
+
+                                cmd.Parameters.Add("@p1", SqlDbType.NVarChar).Value = textBox1.Text;
+                                cmd.Parameters.Add("@p2", SqlDbType.Int).Value = price;
+
+                                cmd.ExecuteNonQuery();
+                                cn.Close();
+                            }
+                            catch (Exception ex)
+                            {
+                                MessageBox.Show(ex.Message);
+                            }
+                            Form1_Load(sender, e);
+                            break;
+                        case "D":
+                            try
+                            {
+                                SqlConnection cn = new SqlConnection();
+                                cn.ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;" +
+                                    "AttachDbFilename=|DataDirectory|Food.mdf;" +
+                                    "Integrated Security=True";
+                                cn.Open();
+                                String sqlstr = "INSERT INTO Drink(Id,Price) VALUES(@p1,@p2)";
+
+                                SqlCommand cmd = new SqlCommand(sqlstr, cn);
+
+                                cmd.Parameters.Add("@p1", SqlDbType.NVarChar).Value = textBox1.Text;
+                                cmd.Parameters.Add("@p2", SqlDbType.Int).Value = price;
+                                cmd.ExecuteNonQuery();
+                                cn.Close();
+                            }
+                            catch (Exception ex)
+                            {
+                                MessageBox.Show(ex.Message);
+                            }
+                            Form1_Load(sender, e);
+                            break;
+                    }
+                    textBox1.Text = "";
+                    textBox2.Text = "";
+                    textBox3.Text = "";
+                }
+                catch
+                {
+                    MessageBox.Show("請重新輸入");
+                }
+            }
+            else
+            {
+                MessageBox.Show("請重新輸入");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            
+            try{
+                SqlConnection cn = new SqlConnection();
+                cn.ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;" +
+                "AttachDbFilename=|DataDirectory|Food.mdf;" +
+                "Integrated Security=True";
+                cn.Open();
+                String sqlstr = "DELETE FROM Food WHERE Id = @p1";
+
+                SqlCommand cmd = new SqlCommand(sqlstr, cn);
+
+                cmd.Parameters.Add("@p1", SqlDbType.NVarChar).Value = comboBox1.Text;
+
+                cmd.ExecuteNonQuery();
+                cn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+            Form1_Load(sender, e);
+            MessageBox.Show("刪除成功");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlConnection cn = new SqlConnection();
+                cn.ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;" +
+                "AttachDbFilename=|DataDirectory|Food.mdf;" +
+                "Integrated Security=True";
+                cn.Open();
+                String sqlstr = "DELETE FROM Drink WHERE Id = @p1";
+
+                SqlCommand cmd = new SqlCommand(sqlstr, cn);
+
+                cmd.Parameters.Add("@p1", SqlDbType.NVarChar).Value = comboBox2.Text;
+
+                cmd.ExecuteNonQuery();
+                cn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            Form1_Load(sender, e);
+            MessageBox.Show("刪除成功");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            food.Clear();
+            for(int i = 0; i < checkedListBox1.Items.Count; i++)
+            {
+                if (checkedListBox1.GetItemChecked(i))
+                {
+                    food.Add(checkedListBox1.Items[i].ToString());
+                }
+            }
+            if (food.Count > 0)
+            {
+                Random rnd = new Random(Guid.NewGuid().GetHashCode());
+                int index = rnd.Next(0, food.Count);
+                DialogResult dr = MessageBox.Show("你選到:" + food[index],"提示",MessageBoxButtons.OKCancel);
+                if(dr == DialogResult.OK) {
+                    int times = 0;
+                    for (int i = 0; i < dataGridView1.RowCount - 1; i++)
+                    {
+                        if (dataGridView1[0, i].Value.Equals(food[index]))
+                        {
+                            times = int.Parse(dataGridView1[2, i].Value.ToString());
+                            times++;
+                            break;
+                        }
+                    }
+                    try
+                    {
+                        SqlConnection cn = new SqlConnection();
+                        cn.ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;" +
+                        "AttachDbFilename=|DataDirectory|Food.mdf;" +
+                        "Integrated Security=True";
+                        cn.Open();
+                        String sqlstr = "UPDATE Food SET times = @p1 WHERE Id = @p2";
+
+                        SqlCommand cmd = new SqlCommand(sqlstr, cn);
+                        cmd.Parameters.Add("@p1", SqlDbType.Int).Value = times;
+                        cmd.Parameters.Add("@p2", SqlDbType.NVarChar).Value = food[index];
+
+                        cmd.ExecuteNonQuery();
+                        cn.Close();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                    Form1_Load(sender, e);
+                }
+                else if(dr == DialogResult.Cancel){
+
+                }
+            }
+            else
+            {
+                MessageBox.Show("請選多一點");
+            }
+            /*
+            String str = "";
+            for(int i = 0; i < food.Count; i++)
+            {
+                str += food.ElementAt(i);
+                str += ",";
+            }
+            MessageBox.Show(str);
+            */
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            drink.Clear();
+            for (int i = 0; i < checkedListBox2.Items.Count; i++)
+            {
+                if (checkedListBox2.GetItemChecked(i))
+                {
+                    drink.Add(checkedListBox2.Items[i].ToString());
+                }
+            }
+            if (drink.Count > 0)
+            {
+                Random rnd = new Random(Guid.NewGuid().GetHashCode());
+                int index = rnd.Next(0, drink.Count);
+                DialogResult dr = MessageBox.Show("你選到:" + drink[index], "提示", MessageBoxButtons.OKCancel);
+                if (dr == DialogResult.OK)
+                {
+                    int times = 0;
+                    for (int i = 0; i < dataGridView2.RowCount - 1; i++)
+                    {
+                        if (dataGridView2[0, i].Value.Equals(drink[index]))
+                        {
+                            times = int.Parse(dataGridView2[2, i].Value.ToString());
+                            times++;
+                            break;
+                        }
+                    }
+                    try
+                    {
+                        SqlConnection cn = new SqlConnection();
+                        cn.ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;" +
+                        "AttachDbFilename=|DataDirectory|Food.mdf;" +
+                        "Integrated Security=True";
+                        cn.Open();
+                        String sqlstr = "UPDATE Drink SET times = @p1 WHERE Id = @p2";
+
+                        SqlCommand cmd = new SqlCommand(sqlstr, cn);
+                        cmd.Parameters.Add("@p1", SqlDbType.Int).Value = times;
+                        cmd.Parameters.Add("@p2", SqlDbType.NVarChar).Value = drink[index];
+
+                        cmd.ExecuteNonQuery();
+                        cn.Close();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                    Form1_Load(sender, e);
+                }
+                else if (dr == DialogResult.Cancel)
+                {
+
+                }
+            }
+            else
+            {
+                MessageBox.Show("請選多一點");
+            }
+        }
+    }
+}
